94e7b315baa925bf5e0a3b8bfb2eaedb1d725f6b
Marc-André Bühler <git@public.mabsoftware.ch>
2016-11-16 23:36:21 +0100

    Added integration tests

---
+++ test/extension.test.ts
@@ -0,0 +1,53 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+import * as gitignoreExtension from '../src/extension';
+
+
+suite('GitignoreRepository', () => {
+
+	test('is getting all .gitignore files', () => {
+
+		const GitHubApi = require('github');
+
+		// Create a Github API client
+		let client = new GitHubApi({
+			version: '3.0.0',
+			protocol: 'https',
+			host: 'api.github.com',
+			//debug: true,
+			pathPrefix: '',
+			timeout: 5000,
+			headers: {
+				'user-agent': 'vscode-gitignore-extension'
+			}
+		});
+
+		// Create gitignore repository
+		let gitignoreRepository = new gitignoreExtension.GitignoreRepository(client);
+
+		return Promise.all([
+			gitignoreRepository.getFiles(),
+			gitignoreRepository.getFiles('Global')
+		])
+		.then((result) => {
+			let files: gitignoreExtension.GitignoreFile[] = Array.prototype.concat.apply([], result);
+
+			// From .
+			let rootItem = files.find(f => f.label === 'VisualStudio');
+			assert.deepEqual(rootItem, {
+				description: 'VisualStudio.gitignore',
+				label: 'VisualStudio',
+				url: 'https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore',
+			});
+
+			// From ./Global
+			let globalItem = files.find(f => f.label === 'VisualStudioCode');
+			assert.deepEqual(globalItem, {
+				label: 'VisualStudioCode',
+				description: 'Global/VisualStudioCode.gitignore',
+				url: 'https://raw.githubusercontent.com/github/gitignore/master/Global/VisualStudioCode.gitignore'
+			});
+		});
+	});
+});
